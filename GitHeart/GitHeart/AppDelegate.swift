6f184dd51a376d3c2dc991d1641e9e434cfaf494
Aleksey Kuznetsov <zummenix@gmail.com>
2021-06-23 19:31:32 +0500

    Add project files

---
+++ GitHeart/GitHeart/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  GitHeart
+//
+//  Created by Aleksey Kuznetsov on 23.06.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+}
+
