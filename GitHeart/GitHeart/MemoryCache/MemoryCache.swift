d76781e60217d0400c0aef901e1db13ffe4ddded
Aleksey Kuznetsov <zummenix@gmail.com>
2021-07-03 14:16:39 +0500

    Add unit tests and doc comments for MemoryCache

--- GitHeart/GitHeart/MemoryCache/MemoryCache.swift
+++ GitHeart/GitHeart/MemoryCache/MemoryCache.swift
@@ -7,23 +7,36 @@
 
 import Foundation
 
+/// A type that provides a size in bytes.
 protocol ByteSizable {
+    /// Size in bytes.
     var byteSize: Int { get }
 }
 
+/// A structure to cache values by a key, stores data in memory.
+///
+/// In case if the new value exceeds the `maxByteSize` the cache will start to remove old values to fit
+/// the new one.
+///
+/// Time complexity to get the value is O(n). Time complexity to set the value in a worst case is O(n),
+/// in a best case is O(1).
 class MemoryCache<Key: Hashable, Value: ByteSizable> {
-    let maxByteSize: Int
     private var list: [(Key, Value)] = []
     private(set) var totalSize: Int = 0
+
+    /// Maximum size of the cache.
+    let maxByteSize: Int
 
     init(maxByteSize: Int) {
         self.maxByteSize = maxByteSize
     }
 
+    /// Returns value for the key or nil if doesn't exist.
     func value(forKey key: Key) -> Value? {
         return index(forKey: key).map { list[$0].1 }
     }
 
+    /// Sets or resets value for the key.
     func set(value: Value?, for key: Key) {
         if let value = value {
             if totalSize + value.byteSize > maxByteSize {
@@ -38,10 +51,15 @@ class MemoryCache<Key: Hashable, Value: ByteSizable> {
             totalSize += value.byteSize
             list.append((key, value))
         } else {
-            evictValue(forKey: key)
+            if let index = index(forKey: key) {
+                // Evict the value.
+                totalSize -= list[index].1.byteSize
+                list.remove(at: index)
+            }
         }
     }
 
+    /// Clears the cache.
     func clear() {
         list.removeAll(keepingCapacity: false)
         totalSize = 0
@@ -49,12 +67,5 @@ class MemoryCache<Key: Hashable, Value: ByteSizable> {
 
     private func index(forKey key: Key) -> Int? {
         return list.lazy.enumerated().reversed().first(where: { $0.1.0 == key })?.offset
-    }
-
-    private func evictValue(forKey key: Key) {
-        if let index = index(forKey: key) {
-            totalSize -= list[index].1.byteSize
-            list.remove(at: index)
-        }
     }
 }
---
+++ GitHeart/GitHeart/MemoryCache/MemoryCacheTests.swift
@@ -0,0 +1,56 @@
+//
+//  MemoryCacheTests.swift
+//  GitHeartTests
+//
+//  Created by Aleksey Kuznetsov on 03.07.2021.
+//
+
+@testable import GitHeart
+import XCTest
+
+extension String: ByteSizable {
+    public var byteSize: Int {
+        return count // For testing purposes we count one character as one byte.
+    }
+}
+
+private func cache(size: Int = 20) -> MemoryCache<String, String> {
+    return MemoryCache(maxByteSize: size)
+}
+
+class MemoryCacheTests: XCTestCase {
+    func testSetsAndGetsValue() throws {
+        let c = cache()
+        c.set(value: "a", for: "b")
+        XCTAssertEqual(c.value(forKey: "b"), "a")
+    }
+
+    func testTotalSizeChanges() {
+        let c = cache()
+        c.set(value: "a", for: "a")
+        c.set(value: "bb", for: "bb")
+        c.set(value: "ccc", for: "ccc")
+        XCTAssertEqual(c.totalSize, 6)
+        c.set(value: nil, for: "ccc")
+        XCTAssertEqual(c.totalSize, 3)
+        c.clear()
+        XCTAssertEqual(c.totalSize, 0)
+    }
+
+    func testEvictsOldValuesAndDoesntOverflow() {
+        let c = cache(size: 2)
+        c.set(value: "a", for: "a")
+        c.set(value: "b", for: "b")
+        c.set(value: "c", for: "c")
+        XCTAssertNil(c.value(forKey: "a"))
+        XCTAssertEqual(c.value(forKey: "c"), "c")
+        XCTAssertEqual(c.totalSize, 2)
+    }
+
+    func testDoesntSetBigValue() {
+        let c = cache(size: 2)
+        c.set(value: "aaa", for: "aaa")
+        XCTAssertNil(c.value(forKey: "aaa"))
+        XCTAssertEqual(c.totalSize, 0)
+    }
+}
