440ebe3f27c049cc892363d083fc6edc27eee798
Aleksey Kuznetsov <zummenix@gmail.com>
2021-07-04 14:08:07 +0500

    Remove unnecessary imports

--- GitHeart/API/UserDetailsProvider.swift
+++ GitHeart/API/UserDetailsProvider.swift
@@ -5,8 +5,6 @@
 //  Created by Aleksey Kuznetsov on 03.07.2021.
 //
 
-import Foundation
-
 /// A type that provides the details of a user.
 protocol UserDetailsProvider {
     /// Requests the details of a user by its `login`.
--- GitHeart/API/UsersListProvider.swift
+++ GitHeart/API/UsersListProvider.swift
@@ -5,8 +5,6 @@
 //  Created by Aleksey Kuznetsov on 03.07.2021.
 //
 
-import Foundation
-
 /// A type that provides the list of users.
 protocol UsersListProvider {
     /// Requests the list of users by a `page` and applying `searchTerm` if not empty.
